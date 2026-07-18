495ef77f4756ec3b43b6f17f80025fce4a0aa1fc
Siddharth Gajbhare <160532097+siddharthgajbhare@users.noreply.github.com>
2026-07-18 10:31:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (87.46%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x < 2) return x;
+
+        long long left = 1;
+        long long right = x / 2;
+        long long ans = 0;
+
+        while (left <= right) {
+            long long mid = left + (right - left) / 2;
+
+            if (mid * mid <= x) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
